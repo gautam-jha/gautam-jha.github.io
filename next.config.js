c02bc2ebab9984b7a35efc337b6ab49b57c67243
Gautam <batman@pop-os.localdomain>
2021-12-22 22:13:23 +0530

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+const debug = process.env.NODE_ENV !== 'production'
+
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  assetPrefix: !debug ? 'https://gautam-jha.github.io/' : '',
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.body.classList.add('loading');
+  })
   return <Component {...pageProps} />
 }
 
