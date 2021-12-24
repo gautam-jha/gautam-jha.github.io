878709483edfa62e5f202dce581eedc740069bdb
Gautam <batman@pop-os.localdomain>
2021-12-25 00:28:10 +0530

    Minor deployment fixes

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -9,38 +9,46 @@ interface Props {
 export default function Banner({ slider }: Props): ReactElement {
     useEffect(() => {
         // instantiate a loader
-        const random = Math.floor(Math.random() * (slider.length));
-        const resource = slider[random];
-        const resourceUrl = resource?.Picturehandle;
         const loader = new THREE.ImageLoader();
-        // load a image resource
-        loader.load(
-            // resource URL
-            resourceUrl,
+        let i = 0;
+        setInterval(() => {
+            const resource = slider[i];
+            const resourceUrl = resource?.Picturehandle;
+            // load a image resource
+            loader.load(
+                // resource URL
+                resourceUrl,
 
-            // onLoad callback
-            (image) => {
-                // use the image, e.g. draw part of it on a canvas
-                const canvas = document.getElementById(
-                    'canvas',
-                ) as HTMLCanvasElement;
-                canvas.height = image.height;
-                canvas.width = image.width;
-                // @eslint-disable-next-line
-                const context = canvas.getContext('2d');
-                if (context) {
-                    context.drawImage(image, 0, 0);
-                }
-            },
+                // onLoad callback
+                (image) => {
+                    // use the image, e.g. draw part of it on a canvas
+                    const canvas = document.getElementById(
+                        'canvas',
+                    ) as HTMLCanvasElement;
+                    canvas.height = image.height;
+                    canvas.width = image.width;
+                    // @eslint-disable-next-line
+                    const context = canvas.getContext('2d');
+                    if (context) {
+                        context.drawImage(image, 0, 0);
+                    }
+                },
 
-            // onProgress callback currently not supported
-            undefined,
+                // onProgress callback currently not supported
+                undefined,
 
-            // onError callback
-            () => {
-                console.error('An error happened.');
-            },
-        );
+                // onError callback
+                () => {
+                    console.error('An error happened.');
+                },
+            );
+            
+            if (i === slider.length -1) {
+                i = 0;
+            }
+            
+            i++;
+        }, 5000);
     }, []);
 
     return <canvas id="canvas"></canvas>;
