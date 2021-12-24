d010fe8239a5f9d3d23ba3dc94fb88be3e41e020
Gautam <batman@pop-os.localdomain>
2021-12-24 22:52:48 +0530

    Deploy Next.js to gh-pages

---
+++ out/_next/static/MeZIO6B3rvwttZ54UwqqT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-e6724761f82d9aba.js","static/css/3c90b12e7de8ade8.css","static/chunks/pages/index-5f7e3c22c9123e28.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
