0af020137adbcbd3c18b9ace529fcf8e73ddbaf1
gautamjha.mailbox@gmail.com <batman@pop-os.localdomain>
2022-01-09 01:21:42 +0530

    Deploy Next.js to gh-pages

---
+++ _next/static/uw4aTXwqgMjQNnv3svZu9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/49afe9504e5f475e.css","static/chunks/pages/index-f49d5d59ba3898bd.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
