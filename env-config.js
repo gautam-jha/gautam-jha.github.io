83008bb9518a21093b434b164cd7a707126dd945
Gautam Jha <gautamjha.mailbox@gmail.com>
2026-03-08 13:13:10 +0530

    reset 0

--- env-config.js
+++
@@ -1,6 +0,0 @@
-const dev = process.env.NODE_ENV !== "production";
-const server = dev ? "http://localhost:3000" : "https://gautam-jha.github.io";
-
-module.exports = {
-  'process.env.BACKEND_URL': server,
-}
--- index.d.ts
+++
@@ -1,1 +0,0 @@
-declare module '@afiniti/webgl-slider';
